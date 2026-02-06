72496b08c696f26dd38fc9b1b8484f5ba171fea0
Marci0112 <>
2026-02-06 11:28:42 +0100

    asd
    asd

--- PHP/explore.php
+++ PHP/explore.php
@@ -7,6 +7,14 @@
     <title>Tradely</title>
 </head>
 <body>
-    
+    <div class="navbar">
+        <div class="title">
+            <h1>Explore.</h1>
+        </div>
+        <div class="icons">
+            <button><img src="../ASSETS/bell.png" alt="" width= "30px";></button>
+            <button><img src="../ASSETS/filter.png" alt="" width="30px";></button>
+        </div>
+    </div>
 </body>
 </html>
