1c792cacfd1b6d604c9d46ca3cf51a5ab3a61e2e
foldyroland <foldyroland@gmail.com>
2026-02-06 08:42:59 +0100

    miafasz
    dgs

---
+++ PHP/welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <title>Tradely</title>
+</head>
+<body>
+    <div class="card">
+        <img src="../ASSETS/welcome-pic.jpg" width="300px">
+        <table>
+            <tr>
+                <th><img src="" alt=""></th>
+                <th><h1>Tradely</h1></th>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
