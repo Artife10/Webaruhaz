7535106771cd85e77348fa0b73623e863b2286bf
foldyroland <foldyroland@gmail.com>
2026-02-06 09:03:50 +0100

    asd
    asd

--- PHP/welcome.php
+++ PHP/welcome.php
@@ -11,11 +11,12 @@
         <img src="../ASSETS/welcome-pic.jpg" width="300px">
         <table>
             <tr>
-                <th><img src="../ASSETS/trade.png" alt="" width="50px"></th>
+                <th><img src="../ASSETS/trade.png" alt="" width="50px"></th> <br>
                 <th><h1>Tradely</h1></th>
-                <p>Join the community of smart <br> buy and sell used goods <br> with ease.</p>
-            </tr>
+            </tr>    
         </table>
-    </div>
+        <p>Join the community of smart <br> buy and sell used goods <br> with ease.</p>
+        <button>Get started</button>
+        <button></button>
 </body>
 </html>
